74d1562091dfed12fad5a2bca91ba420ecacbe68
Hector Ordonez <hjavierog@gmail.com>
2022-11-15 09:57:06 +0700

    Calculating nut transportation

--- src/app/exercise/exercise.component.ts
+++ src/app/exercise/exercise.component.ts
@@ -6,12 +6,14 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./exercise.component.css']
 })
 export class ExerciseComponent implements OnInit {
-  nutAmount: number = 12;
-  distance: number = 10;
-  carryAmount: number = 4;
-  fuel_consumption: number = 10;
+  nutAmount: number = 100;
+  distance: number = 5;
+  carryAmount: number = 25;
+  fuelConsumption: number = 2;
+  totalTransported: number;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
@@ -20,38 +22,65 @@ export class ExerciseComponent implements OnInit {
     let n = +this.nutAmount
     let d = +this.distance
     let c = +this.carryAmount
-    let f = +this.fuel_consumption
+    let f = +this.fuelConsumption
 
-    console.log('Calculating...')
-    console.log('Only using nut amount and carry amount to calculate number of trips...')
-    console.log('(N): ' + this.nutAmount)
-    // console.log('(D): ' + this.distance)
-    console.log('(C): ' + this.carryAmount)
-    // console.log('(F): ' + this.fuel_consumption)
+    this.totalTransported = this.calculate(n, d, c, f)
+  }
 
-    let tripCount = 1
-    while(n > 0)
-    {
-      console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-      console.log('There are ' + n + ' nuts to carry. Making a trip')
+  calculate(n, d, c, f)
+  {
+    let tripCount = 0
+    let totalTransported = 0
+
+    while (n > 0) {
       tripCount++
+
+      this.log()
+      this.log(`There are ${n} nuts to carry. Making a trip`)
       let currentLoad = this.calculateCargo(n, c)
+      let transportedAmount = this.calculateTransport(currentLoad, d, f)
 
       n = n - currentLoad
-      console.log('Taking load out of nut mountain, nuts left:' + n)
+      totalTransported += transportedAmount
+      this.log(`In trip #${tripCount}, ${currentLoad} nuts were taken and ${transportedAmount} arrived to destination`)
+      this.log(`Nuts left: ${n}`)
+      this.log(`Transported ${transportedAmount} to destination, total so far: ${totalTransported}`)
     }
 
-    console.log('Finished in ' + tripCount + ' trips.')
+    this.log(`Finished transporting ${totalTransported} nuts in ${tripCount} trips`)
+
+    return totalTransported
   }
 
   calculateCargo(nutAmount, carryAmount): number {
-    console.log('Calculating next cargo: (N): ' + nutAmount + ', (C): ' + carryAmount)
+    this.log(`Calculating next cargo: (N): ${nutAmount}, (C): ${carryAmount}`)
     if (carryAmount >= nutAmount) {
-      console.log('(C) >= (N): Can carry all that is left. Carrying: ' + nutAmount)
+      this.log(`(C) >= (N): Can carry all that is left. Carrying: ${nutAmount}`)
     } else {
-      console.log('(C) < (N): Cannot carry all of them. Will carry at capacity:' + carryAmount)
+      this.log(`(C) < (N): Cannot carry all of them. Will carry at capacity: ${carryAmount}`)
     }
 
     return carryAmount > nutAmount ? nutAmount : carryAmount
+  }
+
+  calculateTransport(currentLoad: number, distance: number, fuelConsumption: number,) {
+    this.log(`Calculating next transport: (D)istance: ${distance}, (F)uel consumption: ${fuelConsumption}`)
+
+    const totalConsumption = distance * fuelConsumption
+    const transport = currentLoad - totalConsumption
+
+    this.log(`Takes ${totalConsumption} nuts to finish a trip`)
+
+    if (transport < 0) {
+      this.log('This trip did not reach destination')
+      return 0
+    }
+
+    this.log(`Transporting: ${transport} nuts in this trip`)
+    return transport
+  }
+
+  log(msg = '') {
+    console.log(`${msg}.`)
   }
 }
