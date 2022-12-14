4faf2b6fb232a76a9f01e5d70b99010055fe1cd6
Hector Ordonez <hjavierog@gmail.com>
2022-12-14 10:31:44 +0800

    FormArray controls for ingredients

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Data} from "@angular/router";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormArray, FormControl, FormGroup} from "@angular/forms";
 import {RecipeService} from "../recipe.service";
 import {Recipe} from "../recipe.model";
 
 @Component({
+  styleUrls: ['recipe-edit.component.css'],
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
 })
@@ -15,6 +16,10 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute, private recipeService: RecipeService) {
 
+  }
+
+  get controls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls
   }
 
   ngOnInit(): void {
@@ -31,17 +36,30 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = ''
     let recipeImagePath = ''
     let recipeDescription = ''
+    let recipeIngredients = new FormArray([])
 
     if (this.editMode) {
       recipeName = this.editingRecipe.name
       recipeImagePath = this.editingRecipe.imagePath
       recipeDescription = this.editingRecipe.description
+
+      if (this.editingRecipe.ingredients) {
+        for (let ingredient of this.editingRecipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount),
+            })
+          )
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients,
     })
   }
 }
