b04a8998628749ff2d32e1125d119c0c4fe14d89
Hector Ordonez <hjavierog@gmail.com>
2022-12-14 12:27:52 +0800

    Fixing bugs: delete does not redirect

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,7 +11,6 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      1,
       'Tasty Schnitzel',
       'A super-tasty Schnitzel - just awesome!',
       'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
@@ -20,7 +19,6 @@ export class RecipeService {
         new Ingredient('French Fries', 20)
       ]),
     new Recipe(
-      2,
       'Big Fat Burger',
       'What else you need to say?',
       'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
