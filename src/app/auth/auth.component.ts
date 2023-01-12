caf54785a556fc20f3b9bc7c01d115a362ab7f24
Hector Ordonez <hjavierog@gmail.com>
2023-01-12 10:58:10 +0100

    Registering works

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -25,10 +25,22 @@ export class AuthComponent {
   }
 
   private login(authForm: NgForm) {
-    this.authService.login(authForm.value.email, authForm.value.password)
+    this.authService.login(authForm.value.email, authForm.value.password).subscribe(response => {
+      console.log('Got a response from logging in:')
+      console.log(response)
+    }, error => {
+      console.log('Got an error from logging in:')
+      console.log(error)
+    })
   }
 
   private register(authForm: NgForm) {
-    this.authService.register(authForm.value.email, authForm.value.password)
+    this.authService.register(authForm.value.email, authForm.value.password).subscribe(response => {
+      console.log('Got a response from registering:')
+      console.log(response)
+    }, error => {
+      console.log('Got an error from registering:')
+      console.log(error)
+    })
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,35 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {AuthResponseData} from "./auth-response.data";
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
   readonly AUTH_ROOT = 'https://identitytoolkit.googleapis.com/v1/accounts'
-  readonly REGISTER_ENDPOINT = (key: string) => `${this.AUTH_ROOT}:signUp?key=${key}`
-  readonly LOGIN_ENDPOINT = (key: string) => `${this.AUTH_ROOT}:signInWithPassword?key=${key}`
+  readonly KEY = 'AIzaSyCWtZnwnKypFOc8geLHSk9vRhlXq0FBNuA'
+  readonly REGISTER_ENDPOINT = `${this.AUTH_ROOT}:signUp?key=${this.KEY}`
+  readonly LOGIN_ENDPOINT = `${this.AUTH_ROOT}:signInWithPassword?key=${this.KEY}`
 
-  login(email: string, password: string) {
-    console.log(`Attempting to login with ${email} and ${password}`)
+  constructor(private httpClient: HttpClient) {
   }
 
-  register(email: string, password: string) {
+  login(email: string, password: string): Observable<AuthResponseData> {
+    console.log(`Attempting to login with ${email} and ${password}`)
+
+    return this.httpClient.post<AuthResponseData>(this.LOGIN_ENDPOINT, {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    })
+  }
+
+  register(email: string, password: string): Observable<AuthResponseData> {
     console.log(`Attempting to register with ${email} and ${password}`)
+
+    return this.httpClient.post<AuthResponseData>(this.REGISTER_ENDPOINT, {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    })
   }
 }
