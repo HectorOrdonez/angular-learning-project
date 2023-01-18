038f743b1a80cfc3fff7dff2bddb56582c8295fd
Hector Ordonez <hjavierog@gmail.com>
2023-01-18 15:44:01 +0100

    305 Adding Auto-Logout

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,22 +2,23 @@ import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTre
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 import {AuthService} from "./auth.service";
-import {map} from "rxjs/operators";
+import {map, take} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree>{
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
 
-    return this.authService.user.pipe(map(user => {
-      if(!!user)
-      {
-        return true
-      }
+    return this.authService.user.pipe(
+      take(1),
+      map(user => {
+        if (!!user) {
+          return true
+        }
 
-      return this.router.createUrlTree(['/auth'])
-    }))
+        return this.router.createUrlTree(['/auth'])
+      }))
   }
 }
